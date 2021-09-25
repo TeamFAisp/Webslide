74019f3928ea8052b817ae912b740b13d13114ec
DxDHunter <dekushunter11@gmail.com>
2021-09-25 15:46:25 +0300

    updated

--- js/index.js
+++ js/index.js
@@ -1,40 +1,3 @@
-<<<<<<< Updated upstream
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-$('.variable-width').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true
-});
-=======
 $(document).ready(function() {
   
     var $slider = $(".slider"),
@@ -158,4 +121,3 @@ $(document).ready(function() {
     });
     
   });
->>>>>>> Stashed changes
